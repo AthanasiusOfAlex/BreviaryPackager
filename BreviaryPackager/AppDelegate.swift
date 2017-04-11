09eb37ef2fcd254ce74ea5127ad1775616de9789
Louis Melahn <lmelahn@legionaries.org>
2017-04-11 18:47:01 +0200

    Initial Commit

---
+++ BreviaryPackager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BreviaryPackager
+//
+//  Created by Louis Melahn on 4/11/17.
+//  Copyright © 2017 Louis Melahn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BreviaryPackager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BreviaryPackager
+//
+//  Created by Louis Melahn on 4/11/17.
+//  Copyright © 2017 Louis Melahn. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
